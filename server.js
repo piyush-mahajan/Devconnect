f1b8d94aa887a2f4cfe49df27b0f589fd8020e5f
piyush-mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2023-06-10 19:59:00 +0530

    2.4 routes and connectivity to database

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
+
+//connect Database
+connectDB();
 app.get("/", (req, res) => res.send("API Running "));
+
+//Define Routes
+app.use("/api/user", require("./routes/api/user"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
 
 const PORT = process.env.PORT || 5000;
 
