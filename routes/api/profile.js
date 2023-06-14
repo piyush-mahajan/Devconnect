1d2fa67e3ed9ce3f15ee4b6bc6f87489fe7a34c3
piyush-mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2023-06-14 19:39:35 +0530

    profile route for create and update the user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,130 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-//@route        GET api/profile
-// @desc test   route
-// @acces       Public
-router.get("/", (req, res) => res.send("user route"));
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+//@route        GET api/profile/me
+// @desc         get current users profile
+// @acces       Private
+router.get("/me", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+    if (!profile) {
+      return res.status(400).json({ msg: "there is no profile for this user" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error bhai");
+  }
+});
+
+// @route   GET api/users/:id
+// @desc    Get profile by user ID
+// @access  Public
+router.get("/:id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.id,
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) return res.status(400).json({ msg: "Profile not found" });
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+// @route      POST api/profile
+// @desc       Create or update user profile
+// @access     Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // destructure the request
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+    try {
+      // Build profile object
+      const profileFields = {};
+      profileFields.user = req.user.id;
+      if (company) profileFields.company = company;
+      if (website) profileFields.website = website;
+      if (location) profileFields.location = location;
+      if (bio) profileFields.bio = bio;
+      if (status) profileFields.status = status;
+      if (githubusername) profileFields.githubusername = githubusername;
+      if (skills) {
+        profileFields.skills = skills.split(",").map((skill) => skill.trim());
+      }
+      // Build social object
+      profileFields.social = {};
+      if (youtube) profileFields.social.youtube = youtube;
+      if (twitter) profileFields.social.twitter = twitter;
+
+      if (facebook) profileFields.social.facebook = facebook;
+      if (linkedin) profileFields.social.linkedin = linkedin;
+      if (instagram) profileFields.social.instagram = instagram;
+      console.log(profileFields.skills);
+      try {
+        let profile = await Profile.findOne({ user: req.user.id });
+        if (profile) {
+          //update
+          profile = await Profile.findOneAndUpdate(
+            { user: req.user.id },
+            { $set: profileFields },
+            { new: true, upsert: true }
+          );
+          return res.json(profile);
+        }
+
+        // create
+
+        profile = new Profile(profileFields);
+        await profile.save();
+        res.json(profile);
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send("server error bro");
+      }
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("server error mere bhai");
+    }
+  }
+);
+
 module.exports = router;
